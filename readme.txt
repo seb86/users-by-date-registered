c1912a9bfaf866c81d950f3b4fe630f912494255
Sebastien <sebastien@sebs-studio.com>
2014-08-16 04:31:49 +0200

    Initial Release

---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Users by Date Registered ===
+Version: 1.0.0
+Author URI: http://www.sebastiendumont.com
+Author: Sebastien Dumont
+Contributors: sebd86
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=mailme@sebastiendumont.com&currency_code=EUR&amount=&return=&item_name=Buy+me+a+coffee+for+Users+by+Date+Registered+Plugin
+Tags: users, date, filter, admin, Sebastien Dumont
+Requires at least: 3.8
+Tested up to 4.0.0
+Stable Tag: Trunk
+
+Displays a new column displaying the date the user registered and allows you to filter the users by date.
+
+== Description ==
+
+Displays a new column displaying the date the user registered and allows you to filter the users by date.
+
+= Contributing and reporting bugs =
+
+You can contribute code to this plugin via GitHub: [https://github.com/seb86/Users-by-Date-Registered](https://github.com/seb86/Users-by-Date-Registered)
+
+= Support =
+
+Use the WordPress.org forums for community support - I cannot offer support directly for free. If you spot a bug, you can of course log it on [Github](https://github.com/seb86/Users-by-Date-Registered) instead where I can act upon it more efficiently.
+
+If you want help with customisation, hire me if I am available or hire another developer!
+
+= Reviews =
+
+I like stars. Just a simple review from you will help me gain more stars. It only takes a moment of your time.
+
+== Installation ==
+
+= Automatic installation =
+
+Automatic installation is the easiest option as WordPress handles the file transfers itself and you don't even need to leave your web browser. To do an automatic install, log in to your WordPress admin panel, navigate to the Plugins menu and click Add New.
+
+In the search field type "Users by Date Registered" and click Search Plugins. Once you've found the plugin you can view details about it such as the the point release, rating and description. Most importantly of course, you can install it by clicking _Install Now_.
+
+= Manual installation =
+
+The manual installation method involves downloading the plugin and uploading it to your webserver via your favourite FTP application.
+
+* Download the plugin file to your computer and unzip it
+* Using an FTP program, or your hosting control panel, upload the unzipped plugin folder to your WordPress installation's `wp-content/plugins/` directory.
+* Activate the plugin from the Plugins menu within the WordPress admin.
+
+== Changelog ==
+
+= 1.0.0 - 16th August 2014 =
+
+* Initial release
---
+++ users-by-date-registered.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Plugin Name: Users by Date Registered
+ * Plugin URI: http://wordpress.org/plugins/users-by-date-registered
+ * Description: Displays a new column displaying the date the user registered and allows you to filter the users by date.
+ * Version: 1.0.0
+ * Author: Sebastien Dumont
+ * Author URI: http://www.sebastiendumont.com
+ * Author Email: mailme@sebastiendumont.com
+ * Requires at least: 3.8
+ * Tested up to: 4.0.0
+ *
+ * Text Domain: users-by-date
+ * Domain Path: languages
+ * Network: false
+ *
+ * Copyright: (c) 2014 Sebastien Dumont. (mailme@sebastiendumont.com)
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Adds a new column to the users table for 'Date Registered'.
+ *
+ * @since 1.0.0
+ */
+function add_user_table_columns( $column ) {
+  $column['registered'] = __( 'Date Registered', 'users-by-date' );
+
+  return $column;
+}
+add_filter( 'manage_users_columns', 'add_user_table_columns' );
+
+/**
+ * Filters the users table columns to display the 
+ * date and time the users registered.
+ *
+ * @since 1.0.0
+ */
+function modify_user_table_row( $val, $column_name, $user_id ) {
+  $user = get_userdata( $user_id );
+
+  switch( $column_name ) {
+    case 'registered' :
+      $t_time = get_the_time( __( 'Y/m/d g:i:s A' ) );
+      $m_time = $user->user_registered;
+      $time = get_post_time( 'G', true, $user );
+      $time_diff = time() - $time;
+
+      if( $time_diff > 0 && $time_diff < DAY_IN_SECONDS ) {
+        $h_time = sprintf( __( '%s ago' ), human_time_diff( $time ) );
+      }
+      else{
+        $h_time = mysql2date( __( 'd/m/Y' ), $m_time );
+      }
+
+      return '<abbr title="' . $t_time . '">' . apply_filters( 'user_registered_date_column_time', $h_time, $user, $column_name ) . '</abbr>';
+
+      break;
+
+    default:
+      break;
+  }
+}
+add_filter( 'manage_users_custom_column', 'modify_user_table_row', 10, 3 );
+
+/**
+ * Allows you to sort the users table by date order.
+ *
+ * @since 1.0.0
+ */
+function get_sortable_columns() {
+	return array(
+		'registered' => array( 'registered', true )
+	);
+}
+add_filter( 'manage_users_sortable_columns', 'get_sortable_columns' );
+
+/**
+ * Display a monthly dropdown for filtering the users.
+ * Forked from file: class-wp-list-table.php
+ *
+ * @since 1.0.0
+ */
+function users_months_dropdown() {
+	global $wpdb, $wp_locale;
+
+	// Bail if current user cannot manage options
+	if( !current_user_can( 'manage_options' ) )
+		return;
+
+	$months = $wpdb->get_results( $wpdb->prepare( "
+		SELECT DISTINCT YEAR( user_registered ) AS year, MONTH( user_registered ) AS month
+		FROM $wpdb->users
+		ORDER BY user_registered DESC
+		" ) );
+
+	/**
+	 * Filter the 'Months' drop-down results.
+	 *
+	 * @param object $months    The months drop-down query results.
+	 */
+	$months = apply_filters( 'users_months_dropdown_results', $months );
+
+	$month_count = count( $months );
+
+	if ( !$month_count || ( 1 == $month_count && 0 == $months[0]->month ) )
+		return;
+
+	$m = isset( $_GET['userregistered'] ) ? (int) $_GET['userregistered'] : 0;
+	?> 
+	<label for="filter-users-by-date" class="screen-reader-text"><?php _e( 'Filter by date' ); ?></label>
+		<select name="userregistered" id="filter-users-by-date" style="display:inline-block; float:none;">
+			<option<?php selected( $m, 0 ); ?> value="0"><?php _e( 'All dates' ); ?></option>
+			<?php
+			foreach ( $months as $arc_row ) {
+				if ( 0 == $arc_row->year )
+					continue;
+
+				$month = zeroise( $arc_row->month, 2 );
+				$year = $arc_row->year;
+
+				printf( "<option %s value='%s'>%s</option>\n",
+					selected( $m, $year . $month, false ),
+					esc_attr( $arc_row->year . $month ),
+					/* translators: 1: month name, 2: 4-digit year */
+					sprintf( __( '%1$s %2$d' ), $wp_locale->get_month( $month ), $year )
+				);
+			}
+			?>
+		</select> 
+<?php
+		submit_button( __( 'Filter Date' ), 'secondary', 'filter_date_action', false );
+}
+add_action( 'restrict_manage_users', 'users_months_dropdown' );
+
+
+/**
+ * This filters the users table by date.
+ *
+ * @since 1.0.0
+ */
+function extended_user_search( $user_query ){
+	global $wpdb;
+
+	if( isset( $_GET['userregistered'] ) && !empty( $_GET['userregistered'] ) ) {
+		$date = $_GET['userregistered'];
+
+		$year = substr( $date, 0, -2 );
+		$month = substr( $date, -2, 5 );
+
+		switch( $month ) {
+			case 01:
+				$last_day = 31;
+			break;
+			case 02:
+				$last_day = apply_filters( 'users_by_date_month_feb_last_day', 28 );
+			break;
+			case 03:
+				$last_day = 31;
+			break;
+			case 04:
+				$last_day = 30;
+			break;
+			case 05:
+				$last_day = 31;
+			break;
+			case 06:
+				$last_day = 30;
+			break;
+			case 07:
+				$last_day = 31;
+			break;
+			case 08:
+				$last_day = 31;
+			break;
+			case 09:
+				$last_day = 30;
+			break;
+			case 10:
+				$last_day = 31;
+			break;
+			case 11:
+				$last_day = 30;
+			break;
+			case 12:
+				$last_day = 31;
+			break;
+		}
+
+		$user_query->query_where .= " AND user_registered >= '" . $year . "-" . $month . "-01 00:00:00' AND user_registered <= '" . $year . "-" . $month . "-" . $last_day . " 23:59:59'";
+	}
+
+}
+add_action( 'pre_user_query', 'extended_user_search' );
+
+?>
